f33db8d9e154ca3d586b25e6f85cc3bdb1a860a2
DevOp972 <smarakkanjilal@gmail.com>
2022-10-26 12:21:31 +0530

    Fixed Navbar

--- thestrayarmy/thestrayarmy/settings.py
+++ thestrayarmy/thestrayarmy/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'thestrayarmy.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -131,5 +131,5 @@ STATIC_ROOT = os.path.join(VENV_PATH, 'static_root')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
-MEDIA_URL='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
